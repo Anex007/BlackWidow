a746f364ea3f8cc91b13b610805d514c9cfcf429
root <root@localhost.localdomain>
2018-01-01 22:14:41 -0500

    Added main Files

---
+++ black-widow.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+import urllib
+import argparse
+from web_crawler import Spider
+
+def main():
+    parser = argparse.ArgumentParser()
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('-v', '--verbose', help='Verbose Output', action="store_true")
+    group.add_argument('-q', '--quiet', help='Be Quiet, My mom\'s looking', action="store_true")
+    parser.add_argument('url', help='The website to crawl')
+    parser.add_argument('-t', '--threads', help='The number of threads the crawler will run on', default=3, type=int)
+
+    args = parser.parse_args()
+
+    if not (args.url.startswith('http://') or args.url.startswith('https://')):
+        print('[-] Url should be in [http|https]:// format')
+        return
+
+    verbose = 2
+    
+    if args.quiet:
+        verbose = 1
+    elif args.verbose:
+        verbose = 3
+
+    threads = []
+
+    for i in range(args.threads):
+        spider = Spider(args.url ,str(i), verbose)
+        spider.start()
+        threads.append(spider)
+
+    for i in threads:
+        i.join()
+
+
+if __name__ == "__main__":
+    main()
---
+++ web_crawler.py
@@ -0,0 +1,105 @@
+import threading
+import urllib.request as urlL
+from bs4 import BeautifulSoup
+import time
+
+def get_tld(url):
+    tld = ''
+    dm = url.split('/')[2]
+    tld+=dm.split('.')[-2]
+    tld+='.'
+    tld+=dm.split('.')[-1]
+    return tld
+
+ALL = 3
+SOME = 2
+NONE = 1
+
+
+class Spider(threading.Thread):
+    verbose = SOME
+    url = ''
+    tld = ''
+    crawled = set()
+    to_crawl = set()
+    FOUND = False
+
+    def get_all_links(page):
+        links = []
+        soup = BeautifulSoup(page.decode('utf-8', 'ignore'), 'lxml')
+        for link in soup.findAll('a'):
+            if Spider.tld in link or link['href'].startswith('/'):
+                url = link['href']
+                if url.startswith('/'):
+                    url = Spider.url+link['href']
+                links.append(url)
+
+        return links
+
+    def get_page(url):
+        try:
+            web_p = urlL.urlopen(url)
+        except Exception:
+            return bin('')
+        return web_p.read()
+
+    def has_file(page):
+        soup = BeautifulSoup(page.decode('utf-8', 'ignore'), 'lxml')
+        for inpu in soup.findAll('input'):
+            if inpu['type'].lower() == 'file':
+                return True
+
+        return False
+
+    def __init__(self, main_url, name, _verbose=SOME):
+        threading.Thread.__init__(self)
+        Spider.url = main_url
+        Spider.verbose = _verbose
+        self.name = name
+        Spider.tld = get_tld(main_url)
+        self.i = 0
+
+    def log(self, _str, lvl):
+        if self.verbose >= lvl:
+             print(_str)
+    
+    def log_garb(self, _str):
+        self.i+=1
+        if self.i%(18/self.verbose) == 0:
+            print(_str)
+
+    def spi_init(self):
+        self.log("[*] Staring the thread "+self.name, SOME)
+        links = Spider.get_all_links(Spider.get_page(Spider.url))
+        Spider.to_crawl.update(links)
+
+    def stage_2(self):
+        for url in Spider.crawled:
+            if Spider.has_file(Spider.get_page(url)):
+                self.log("\033[0;32m[+] File Upload Found on "+url+'\033[0m' , NONE)
+                Spider.FOUND = True
+
+    def run(self):
+        self.spi_init()
+        s = 0
+        self.log('[*] Starting the Main Spider for '+self.name, ALL)
+        while True:
+            try:
+                url = Spider.to_crawl.pop()
+                links = Spider.get_all_links(Spider.get_page(url))
+                Spider.to_crawl.update(links)
+                Spider.crawled.add(url)
+                self.log_garb("[i] Crawled "+url)
+                s = 0
+            except KeyError:
+                time.sleep(1)
+                if s == 2:
+                    break
+                s += 1
+            
+        self.log('[*] Looking for File Uploads', SOME)
+        self.stage_2()
+
+        if not Spider.FOUND:
+            self.log('[-] We are Sorry to inform you that we were not able to Find any File Upload', NONE)
+
